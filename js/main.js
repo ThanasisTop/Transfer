0848355519bde874fba18b2633405a14c540389d
Athanasios Toptsios <atoptsios@allweb.gr>
2023-04-26 11:50:48 +0300

    instagram link & limobus image change

--- js/main.js
+++ js/main.js
@@ -309,22 +309,8 @@
 		'timeFormat': 'H:i',
 		'step':15});
 
-	$('.dropdown-toggle').dropdown();
+	
 	
 
-$(function() {
-  var selectedClass = "";
-  $(".filter").click(function(){
-    selectedClass = $(this).attr("data-rel");
-    $("#gallery").fadeTo(100, 0.1);
-    $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-    setTimeout(function() {
-      $("."+selectedClass).fadeIn().addClass('animation');
-      $("#gallery").fadeTo(300, 1);
-    }, 300);
-  });
-});
-    
-	
 })(jQuery);
 
